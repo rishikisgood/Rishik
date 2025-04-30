370907238df3cc322c99d80be187e9325aef8921
rishikisgood <Kaulrishik15@gmail.com>
2025-04-30 16:57:18 +0530

    Add files via upload

---
+++ First Doc.py
@@ -0,0 +1,17 @@
+print("This is my first Doc of Python!!!!")
+print(1234567890)
+#-----------------------------------------------
+print("Hello python\n")
+print("hello",5)
+print("welcome to", end= "*")
+x=123
+y="z"
+print(x)
+print(y)
+
+Rishik="Rishik"
+print(Rishik)
+
+name = input("enter your name: ")
+
+print("\nHello", name, "\nWelcome to codingal")
